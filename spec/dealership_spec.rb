62dc731c121bf33b4b7a94783ebd10b58bca1cf6
Jessica Joly & Jennifer Bell <student@epicodus.com>
2015-08-12 11:15:02 -0700

    Add third spec to access list of cars in dealership

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -17,4 +17,11 @@ describe(Dealership) do
     end
   end
 
+  describe("#cars")do
+    it("initially returns an empty array of cars for the dealership") do
+      test_dealership = Dealership.new("Andrea's Used Cars")
+      expect(test_dealership.cars()).to(eq([]))
+    end
+  end
+
 end
