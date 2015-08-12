58ab7aba36cc0021f323fdab9c1e164704aab9a9
Jessica Joly & Jennifer Bell <student@epicodus.com>
2015-08-12 15:16:54 -0700

    Second integration spec pass to view vehicles list with saved entry

--- spec/vehicle_integration_spec.rb
+++ spec/vehicle_integration_spec.rb
@@ -12,4 +12,12 @@ describe('the path to add a new vehicle', {:type => :feature}) do
   click_button('Add Vehicle')
   expect(page).to have_content ('Success!')
   end
+
+describe('the path to view the vehicles list with saved entry ', {:type => :feature}) do
+  it('displays vehicle list with saved entry') do
+  visit('/vehicles')
+  expect(page).to have_content ("Prius")
+  end
+end
+  
 end
