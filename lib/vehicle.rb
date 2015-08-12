beb2a67c306ab02188ececa7a8551376692aff3c
Jessica Joly & Jennifer Bell <student@epicodus.com>
2015-08-12 14:45:10 -0700

    Vehicle app works with Sinatra but no integration specs

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,11 +1,11 @@
 class Vehicle
-  @@all_vehicles = []
+  @@vehicles = []
 
   define_method(:initialize) do |make, model, year|
     @make = make
     @model = model
     @year = year
-    @id = @@all_vehicles.length().+(1)
+    @id = @@vehicles.length().+(1)
   end
 
   define_method(:make) do
@@ -30,16 +30,16 @@ class Vehicle
   end
 
   define_singleton_method(:clear) do
-    @@all_vehicles = []
+    @@vehicles = []
   end
 
   define_singleton_method(:all) do
-    @@all_vehicles
+    @@vehicles
   end
 
   define_singleton_method(:find) do |identification|
     found_vehicle = nil
-    @@all_vehicles.each() do |vehicle|
+    @@vehicles.each() do |vehicle|
       if vehicle.id().eql?(identification.to_i())
         found_vehicle = vehicle
       end
@@ -48,7 +48,7 @@ class Vehicle
   end
 
   define_method(:save) do
-    @@all_vehicles.push(self)
+    @@vehicles.push(self)
   end
 
 end
---
+++ spec/vehicle_integration_spec.rb
@@ -0,0 +1,7 @@
+require 'capybara/rspec'
+require './app'
+Capybara.app = Sinatra::Application
+set(:show_exceptions,false)
+
+describe ()
+end
