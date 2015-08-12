2b4a8507be1bef158f0f88815aef07001f8139c2
Jessica Joly & Jennifer Bell <student@epicodus.com>
2015-08-12 15:25:42 -0700

    Integration spec pass for display of individual vehicle information

--- spec/vehicle_integration_spec.rb
+++ spec/vehicle_integration_spec.rb
@@ -13,11 +13,19 @@ describe('the path to add a new vehicle', {:type => :feature}) do
   expect(page).to have_content ('Success!')
   end
 
-describe('the path to view the vehicles list with saved entry ', {:type => :feature}) do
+describe('view the vehicles list with saved entry ', {:type => :feature}) do
   it('displays vehicle list with saved entry') do
   visit('/vehicles')
   expect(page).to have_content ("Prius")
   end
 end
-  
+
+describe('the path to view more information about a specific vehicle',{:type => :feature})do
+  it('displays more detailed information about a specific vehicle') do
+  visit('/vehicles/1')
+  expect(page).to have_content("Toyota Prius 2000")
+  end
+end
+
+
 end
