d677f987bcdbf7dfba7d8dc49a1ec2663328e9af
Jessica Joly & Jennifer Bell <student@epicodus.com>
2015-08-12 15:02:37 -0700

    First integration spec to add user entry to vehicle list pass

--- spec/vehicle_integration_spec.rb
+++ spec/vehicle_integration_spec.rb
@@ -1,7 +1,15 @@
-require 'capybara/rspec'
-require './app'
+require ('capybara/rspec')
+require ('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions,false)
 
-describe ()
+describe('the path to add a new vehicle', {:type => :feature}) do
+  it('processes the user entry and adds it to the vehicle list') do
+  visit('/vehicles/new')
+  fill_in('make', :with => "Toyota")
+  fill_in('model', :with => "Prius")
+  fill_in('year', :with => 2000)
+  click_button('Add Vehicle')
+  expect(page).to have_content ('Success!')
+  end
 end
