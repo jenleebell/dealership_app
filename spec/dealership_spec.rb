80804c7d663af4560cf74e246278561a9f3e4202
Jessica Joly & Jennifer Bell <student@epicodus.com>
2015-08-12 11:01:14 -0700

    Add first spec to return name of dealership

---
+++ spec/dealership_spec.rb
@@ -0,0 +1,13 @@
+require('rspec')
+require('dealership')
+
+describe(Dealership) do
+  describe("#name") do
+    it("returns the name of the dealership") do
+      test_dealership = Dealership.new("Andrea's Used Cars")
+      expect(test_dealership.name()).to(eq("Andrea's Used Cars"))
+    end
+  end
+
+
+end
